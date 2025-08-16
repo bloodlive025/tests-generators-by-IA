8b8577e1c251d0e8ce947fc7a2a04b0b05e86cf7
HumbleG0d <humbleg0d7642@gmail.com>
2025-08-16 11:45:01 -0500

    🔨 [ADD] componnet login and page for using login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { useState } from "react"
+import { LoginComponent } from "@/app/components/Login"
+
+export default function LoginPage() {
+  const handleSubmit = () => {
+    // Aquí iría la lógica de autenticación
+  }
+
+  return (
+    <LoginComponent />
+  )
+}
